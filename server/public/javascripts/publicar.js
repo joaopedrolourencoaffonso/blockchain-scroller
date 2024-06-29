62b44c4357f5f937c041f77cddb7f37c586dc6ec
Joao Pedro <joaoaffonso@id.uff.br>
2024-06-29 17:06:05 -0300

    Fornecendo a ABI do contrato como json para permitir execução do lado do cliente

--- server/public/javascripts/publicar.js
+++ server/public/javascripts/publicar.js
@@ -4,155 +4,21 @@ let provider = new ethers.providers.Web3Provider(window.ethereum);
 let signer
 
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractABI = [
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "primeiroAutor",
-        "type": "address"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "constructor"
-  },
-  {
-    "anonymous": false,
-    "inputs": [
-      {
-        "indexed": true,
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      },
-      {
-        "indexed": false,
-        "internalType": "string",
-        "name": "titulo",
-        "type": "string"
-      },
-      {
-        "indexed": false,
-        "internalType": "string",
-        "name": "conteudo",
-        "type": "string"
-      }
-    ],
-    "name": "PostEvent",
-    "type": "event"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "address",
-        "name": "novoAutor",
-        "type": "address"
-      }
-    ],
-    "name": "addAutor",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getId",
-    "outputs": [
-      {
-        "internalType": "uint256",
-        "name": "",
-        "type": "uint256"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "getStatus",
-    "outputs": [
-      {
-        "internalType": "string",
-        "name": "",
-        "type": "string"
-      }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "_titulo",
-        "type": "string"
-      },
-      {
-        "internalType": "string",
-        "name": "_conteudo",
-        "type": "string"
-      }
-    ],
-    "name": "publicaPost",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "uint256",
-        "name": "inicio",
-        "type": "uint256"
-      },
-      {
-        "internalType": "uint256",
-        "name": "fim",
-        "type": "uint256"
-      }
-    ],
-    "name": "retornaPost",
-    "outputs": [
-      {
-        "components": [
-          {
-            "internalType": "uint256",
-            "name": "id",
-            "type": "uint256"
-          },
-          {
-            "internalType": "string",
-            "name": "titulo",
-            "type": "string"
-          },
-          {
-            "internalType": "string",
-            "name": "conteudo",
-            "type": "string"
-          }
-        ],
-        "internalType": "struct Scroller.Post[]",
-        "name": "",
-        "type": "tuple[]"
-      }
-    ],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      {
-        "internalType": "string",
-        "name": "novoStatus",
-        "type": "string"
-      }
-    ],
-    "name": "setStatus",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+
+let contractABI;
+
+// Fetch the contract ABI from the URL
+fetch('/js/contractABI.json')
+  .then(response => response.json())
+  .then(data => {
+    contractABI = data; // Assuming the ABI is an array or object
+    // Now you can use the contractABI in your code
+    //console.log(contractABI);
+  })
+  .catch(error => {
+    console.error('Error fetching contract ABI:', error);
   }
-];
+);
 
 async function connectMetamask() {
   await provider.send("eth_requestAccounts",[]);
